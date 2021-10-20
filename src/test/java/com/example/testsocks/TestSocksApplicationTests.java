39e66267febcff78cd546793f8d46ac9406f86a6
allegator-e <61875857+allegator-e@users.noreply.github.com>
2021-10-21 01:19:16 +0300

    v1.1

---
+++ src/test/java/com/example/testsocks/TestSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
